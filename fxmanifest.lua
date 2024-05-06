480af24695df5ca5e14da29258ee4c6b6440f360
azutake <azusa_takemae@dwango.co.jp>
2024-05-06 19:58:43 +0900

    fix by yuka

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,29 +1,29 @@
-fx_version 'cerulean'
-game 'gta5'
+-- fx_version 'cerulean'
+-- game 'gta5'
 
-description 'QB-Traphouse'
-version '2.1.1'
+-- description 'QB-Traphouse'
+-- version '2.1.1'
 
-ui_page 'html/index.html'
+-- ui_page 'html/index.html'
 
-shared_scripts {
-    'config.lua',
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua',
-    'locales/*.lua'
-}
+-- shared_scripts {
+--     'config.lua',
+--     '@qb-core/shared/locale.lua',
+--     'locales/en.lua',
+--     'locales/*.lua'
+-- }
 
-client_script {
-    'client/*.lua',
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/CircleZone.lua',
-}
+-- client_script {
+--     'client/*.lua',
+--     '@PolyZone/client.lua',
+--     '@PolyZone/BoxZone.lua',
+--     '@PolyZone/CircleZone.lua',
+-- }
 
-server_script 'server/*.lua'
+-- server_script 'server/*.lua'
 
-files {
-    'html/*'
-}
+-- files {
+--     'html/*'
+-- }
 
-lua54 'yes'
+-- lua54 'yes'
